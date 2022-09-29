85c740b907e6ac9a2aac4a0cb412f6fa9b7e3349
shourovahmd <moh.abuhanifa@gmail.com>
2022-09-29 07:37:43 +0600

    exercise

---
+++ Exercise.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Exercise = () => {
+    return (
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, officia.
+        </div>
+    );
+};
+
+export default Exercise;
--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        <button className='bg-blue-500'>Click</button>
+    <div className='flex m-10 gap-5'>
+      <div className='w-9/12 grid-cols-3 border'>
+       
+      </div>
+      <div className='w-3/12 grid-cols-1 border'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quam odio voluptatibus beatae debitis aut libero, minus alias fugit reiciendis soluta vero molestiae delectus iste labore quos optio cum quis.
       </div>
     </div>
   );
